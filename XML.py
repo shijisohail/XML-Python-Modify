a02be951e2ea39200fc5af482f037109f67aadc6
shijisohail <shijisohail786@gmail.com>
2022-05-26 00:43:05 +0500

    load feature eanbled

--- XML.py
+++ XML.py
@@ -1,5 +1,8 @@
 import os
-import pdb 
+import itertools
+import threading
+import time
+import sys
 import xml.etree.ElementTree as ET
 
 ET.register_namespace("","http://www.github/cliffe/SecGen/scenario")
@@ -10,6 +13,26 @@ newfile = ("Updated.xml")
 tree = ET.parse('custom.xml')
 root = tree.getroot()
 
+def load():
+    done = False
+    #here is the animation
+    def animate():
+        for c in itertools.cycle(['|', '/', '-', '\\']):
+            if done:
+                break
+            sys.stdout.write('\rloading ' + c)
+            sys.stdout.flush()
+            time.sleep(0.1)
+        sys.stdout.write('\rDone!     ')
+        mainProg()
+
+
+    t = threading.Thread(target=animate)
+    t.start()
+
+    #long process here
+    time.sleep(5)
+    done = True
 #!ET
 
 def printAllbyET():
@@ -29,7 +52,7 @@ def vuln():
         print("Tag you want to change ",x.tag,"--->",x.attrib) # To display selected Ta
         changes = str(input('\nEnter Your changed tag : '))
         x.attrib['module_path'] = f'{changes}'
-        
+    load()
     tree.write(newfile)
     
     
@@ -39,26 +62,23 @@ def encoders():
     for x in tree.findall(".//{http://www.github/cliffe/SecGen/scenario}encoder"):
         print(x.tag,"--->",x.attrib)
     
-    ch = input('\nEnter Tag you want to display : ')
-    for x in root.findall(f'.//encoder[@name="{ch}"]'):
-        print(x.tag,"--->",x.attrib) # To display selected Ta
-        
+    ch = input('\nEnter Tag you want to change : ')
+    for x in root.findall(f".//{{http://www.github/cliffe/SecGen/scenario}}encoder[@name='{ch}']"):
+        print("Tag you want to change ",x.tag,"--->",x.attrib) # To display selected Ta
         changes = str(input('\nEnter Your changed tag : '))
         x.attrib['name'] = f'{changes}'
-        
     tree.write(newfile)
  
  
 def generator():
     print("\n\n******************Generator*****************\n\n")
     
-    for x in tree.findall(".//{http://www.github/cliffe/SecGen/scenario}value"):
-        print(x.tag,"--->",x.text)
+    for x in tree.findall(".//{http://www.github/cliffe/SecGen/scenario}generator"):
+        print(x.tag,"--->",x.attrib)
     
-    ch = input('\nEnter Tag you want to display : ')
-    for x in root.findall(f'.//generator[@type="{ch}"]'):
-        print(x.tag,"--->",x.attrib) # To display selected Ta
-        
+    ch = input('\nEnter Tag you want to change : ')
+    for x in root.findall(f".//{{http://www.github/cliffe/SecGen/scenario}}generator[@type='{ch}']"):
+        print("Tag you want to change ",x.tag,"--->",x.attrib) # To display selected Ta
         changes = str(input('\nEnter Your changed tag : '))
         x.attrib['type'] = f'{changes}'
         
@@ -67,45 +87,58 @@ def generator():
     
 def input1():
     print("\n\n******************Input*****************\n\n")
-    for x in tree.findall(".//input"):
+    for x in tree.findall(".//{http://www.github/cliffe/SecGen/scenario}input"):
             print(x.tag,"--->",x.attrib)
     
-    ch = input('\nEnter Tag you want to display : ')
-    for x in root.findall(f'.//input[@into_datastore="{ch}"]'):
-        print(x.tag,"--->",x.attrib) # To display selected Ta
-        
-        changes = str(input('\nEnter the tag you want to change : '))
-        x.attrib['into_datastore'] = f'{changes}'
+    ch = input('\nEnter Tag you want to change : ')
+    for x in root.findall(f".//{{http://www.github/cliffe/SecGen/scenario}}input[@into='{ch}']"):
+        print("Tag you want to change ",x.tag,"--->",x.attrib) # To display selected Ta
+        changes = str(input('\nEnter Your changed tag : '))
+        x.attrib['into'] = f'{changes}'
         
     tree.write(newfile)
         
-
-print("""   
-    Enter 1 to Display All TAGS ATTRIBUTES and TEXTS
-    Enter 2 for Vulnerabilities
-    Enter 3 for Encoders
-    Enter 4 for Values
-    Enter 5 for Input
-    Enter 6 to display all the tree
-    """)
-
-x = 1000
-
-while x!=0:
-    x = int(input("\nYour Choice  : "))
-    if x==1:
-        printAllbyET()
-    elif x==2:
-        vuln()
-    elif x==3:
-        encoders()
-    elif x==4:
-        generator()
-    elif x==5:
-        input1()
-    elif x==6:
-        ET.dump(tree)
-    else:
-        print("\nInvalid Choice")
+def dataStore():
+    print("\n\n******************Input*****************\n\n")
+    for x in tree.findall(".//{http://www.github/cliffe/SecGen/scenario}datastore"):
+            print(x.tag,"--->",x.attrib)
+    
+    ch = input('\nEnter Tag you want to change : ')
+    for x in root.findall(f".//{{http://www.github/cliffe/SecGen/scenario}}datastore[@access='{ch}']"):
+        print("Tag you want to change ",x.tag,"--->",x.attrib) # To display selected Ta
+        changes = str(input('\nEnter Your changed tag : '))
+        x.attrib['access'] = f'{changes}'
         
-# os.write(pathM+)
+    tree.write(newfile)
+def mainProg():
+    print("""   
+        Enter 1 to Display All TAGS ATTRIBUTES and TEXTS
+        Enter 2 for Vulnerabilities
+        Enter 3 for Encoders
+        Enter 4 for Generators
+        Enter 5 for Input
+        Enter 6 to display all the tree
+        """)
+
+    x = 1000
+
+    while x!=0:
+        x = int(input("\nYour Choice  : "))
+        if x==1:
+            printAllbyET()
+        elif x==2:
+            vuln()
+        elif x==3:
+            encoders()
+        elif x==4:
+            generator()
+        elif x==5:
+            input1()
+        elif x==6:
+            ET.dump(tree)
+        else:
+            print("\nInvalid Choice")
+            
+    # os.write(pathM+)
+
+mainProg()
